ab6421d90738207ab1c392e164cae44b402c660a
Erlandas Zelvys <erlandasz@gmail.com>
2025-06-26 16:42:45 +0300

    adjust edit company

--- app/Filament/Resources/CompanyResource/Pages/EditCompany.php
+++ app/Filament/Resources/CompanyResource/Pages/EditCompany.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\CompanyResource\Pages;
 
 use App\Filament\Resources\CompanyResource;
 use Filament\Actions;
+use Filament\Actions\Action;
 use Filament\Resources\Pages\EditRecord;
 
 class EditCompany extends EditRecord
@@ -14,6 +15,59 @@ class EditCompany extends EditRecord
     {
         return [
             Actions\DeleteAction::make(),
+            Action::make('copyWebsite')
+                ->label('Copy Website')
+                ->action(function () {
+                    $website = $this->prepareWebsiteForCopy($this->record->website);
+                    $this->copyToClipboard($website);
+                }),
+            Action::make('copyProfileOrShortProfile')
+                ->label('Copy Profile')
+                ->action(function () {
+                    $copyText = $this->record->short_profile ?? $this->record->profile ?? '';
+                    $this->copyToClipboard($copyText);
+                }),
+            Action::make('copyLogoUrl')
+                ->label('Copy Logo URL')
+                ->action(function () {
+                    $logoUrl = $this->getLogoUrlForCopy();
+                    $this->copyToClipboard($logoUrl);
+                }),
         ];
+    }
+
+    protected function prepareWebsiteForCopy(string $website): string
+    {
+        if (! $website) {
+            return '';
+        }
+
+        $website = rtrim($website, '/');
+        $website = preg_replace('#^https?://#', '', $website);
+        $website = preg_replace('#^www\.#', '', $website);
+        $website = 'www.'.$website;
+
+        return $website;
+    }
+
+    protected function getLogoUrlForCopy(): string
+    {
+        $cloudinaryUrl = $this->record->cloudinary_url ?? null;
+        $logoName = $this->record->logo_name ?? null;
+
+        if ($cloudinaryUrl) {
+            return $cloudinaryUrl;
+        }
+
+        if ($logoName) {
+            return "https://sachsevent.com/images/compLogos/{$logoName}";
+        }
+
+        return '';
+    }
+
+    protected function copyToClipboard(string $value): void
+    {
+        $this->notify('success', 'Copied to clipboard!');
     }
 }
