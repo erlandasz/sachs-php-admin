ab9a52dc3dcd1ea44072c28a18cedf285e18c06e
Erlandas Zelvys <erlandasz@gmail.com>
2025-08-28 16:36:09 +0300

    add airtalbe webhook

--- app/Http/Controllers/Webhook.php
+++ app/Http/Controllers/Webhook.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Airtable;
 use App\Models\Company;
 use App\Models\Person;
 use Illuminate\Http\Request;
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,9 @@
 <?php
 
 use App\Http\Controllers\CronController;
+use App\Services\AirtableService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Services\AirtableService;
 
 Route::get('/trigger', function (Request $request) {
     return 'hello-world';
